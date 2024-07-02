6926060447485eef250d00c7301452e842e529be
dzoniradilica <dzonidzonatan2@gmail.com>
2024-07-02 13:30:52 +0200

    Added base settigns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Login() {
   return (
-    <form action="">
-      <label htmlFor="">Email</label>
-      <input type="text" name="" id="" />
+    <form>
+      <div>
+        <label htmlFor="login_email">Email:</label>
+        <input type="email" name="login_email" id="login_email" />
+      </div>
     </form>
   );
 }
